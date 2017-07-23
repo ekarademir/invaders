ef35e2cbe19899cad8cc59879cb0b75069462e52
Ertugrul Karademir <ekarademir@gmail.com>
2017-07-23 21:20:17 +0100

    Boilerplate app added

---
+++ index.js
@@ -0,0 +1,14 @@
+const p5 = require("p5")
+
+let sketch = function (p5) {
+
+    p5.setup = () => {
+        let canvas = p5.createCanvas(p5.displayWidth, p5.displayHeight)
+        p5.smooth()
+    }
+
+    p5.draw = () => {
+        p5.background(255,10)
+    }
+
+}
