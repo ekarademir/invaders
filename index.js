3c1f0e05c3b8e80c15ee2ee7825927ecf878a7dc
Ertugrul Karademir <ekarademir@gmail.com>
2017-07-27 21:18:11 +0100

    First rectangle

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const p5 = require('p5')
 
-const pixel = 10
 
-const boardWidth  = 200
-const boardHeight = 300
+const bw = 40
+const bh = 60
 
+let p = 10
+let canvas
 
 let sketch = function (p5) {
 
     p5.setup = () => {
-        let canvas = p5.createCanvas(p5.displayWidth, p5.displayHeight)
+        canvas = p5.createCanvas(p5.displayWidth, p5.displayHeight)
+
+        p = Math.min( [p5.displayWidth/boardWidth, p5.displayHeight/boardHeight] )
+
         p5.smooth()
     }
 
     p5.draw = () => {
         p5.background(0)
         
-        p5.rectangle()
+        p5.rectangle(1*p, 1*p, (bw-2)*p, (bh-2)*p )
     }
 
 }
