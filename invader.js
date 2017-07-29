27f764bfb54bbbf0c88d7f1e405e32042ef8eb3e
Ertugrul Karademir <ekarademir@gmail.com>
2017-07-29 07:50:20 +0100

    Basic enemy ship implementation

--- invader.js
+++ invader.js
@@ -1,4 +1,4 @@
-module.exports = exports = {Board, Enemy}
+module.exports = exports = {}
 
 /**
  * Game board.
@@ -7,22 +7,70 @@ module.exports = exports = {Board, Enemy}
  * @param {number} height 
  */
 
-function Board(width, height){
+exports.Board = function Board(width, height){
   this.width = width
   this.height = height
 
   /**
    * @param {p5} p5
    */
-  this.draw = (p5) => {
+  this.draw = function(p5) {
+
     p5.stroke(255)
     p5.strokeWeight(1)
     p5.noFill()
     p5.rect( 10, 10, (this.width-20), (this.height-20) )
+
   }
 }
 
-function Enemy() {
+/**
+ * Enemy ships
+ * 
+ * @param {Board}     board - The board this enemy belogs to
+ * @param {p5.Vector} pos   - Position vector of the centre
+ * @param {width}  width
+ * @param {height} height
+ */
+exports.Enemy = function Enemy(pos, board) {
+
+  this.board = board
+  this.pos = pos
+  this.width  = 20
+  this.height = 7
+
+  /**
+   * @param {p5} p5
+   */
+  this.draw = function(p5) {
+    
+    p5.fill(50)
+    p5.noStroke()
+    p5.rect(
+      this.pos.x-this.width/2, 
+      this.pos.y-this.height/2, 
+      this.width, this.height
+    )
+
+  }
+
+  /**
+   * @param {p5.Vector} to
+   */
+  this.move = function(to) {
+
+    this.pos.add(to)
+
+  }
+
+  /**
+   * @param {Board} _b
+   */
+  this.setBoard = function(_b) {
+    
+    this.board = _b
+
+  }
 
 }
 
